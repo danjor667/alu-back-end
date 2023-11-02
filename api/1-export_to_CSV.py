3c5af29717f8eaacc0e2c4d778afe56cd575a9f3
danjor667 <jordannguepi@gmail.com>
2023-11-02 09:29:35 +0200

    fix

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,9 +2,9 @@
 """
 gathing data from an api
 """
+import csv
 import requests
 import sys
-import csv
 
 if __name__ == "__main__":
     user_id = int(sys.argv[1])
@@ -21,16 +21,14 @@ if __name__ == "__main__":
 
     for user in users:
         if user.get("id") == user_id:
-            name = user.get("name")
-
+            name = user.get("username")
 
     with open(file, 'a', newline='') as f:
         header = ["userId", "name", "completed", "title"]
         writer = csv.DictWriter(f, fieldnames=header, quoting=csv.QUOTE_ALL)
 
-        for  todo in todos:
+        for todo in todos:
             if todo.get("userId") == user_id:
-                todo.update({"name":name})
+                todo.update({"name": name})
                 del todo["id"]
                 writer.writerow(todo)
-
